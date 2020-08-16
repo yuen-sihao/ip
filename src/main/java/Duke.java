a0b5188188f4ba1f335acaccb1af1bcf47611cc3
Yuen Si Hao <e0407226@u.nus.edu>
2020-08-16 14:24:09 +0800

    Add Increment: Level 0 - Greet
    A skeletal version of the Duke that simply greets the user and exits.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,5 +6,11 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println("___________________________________");
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
+        System.out.println("___________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("___________________________________");
     }
 }
