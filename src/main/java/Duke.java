a7e20cb86795b3f9dd46472e40df23de9725c4c9
Yuen Si Hao <e0407226@u.nus.edu>
2020-08-22 11:01:50 +0800

    Add ability to store user text and display them
    Duke now has the ability to store text entered by the user and display
    them when requested.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -13,15 +13,25 @@ public class Duke {
         System.out.println("What can I do for you?");
         System.out.println("___________________________________");
         String command;
+        String[] commands = new String[100];
+        int numberOfCommands = 0;
         while (true) {
             Scanner input = new Scanner(System.in);
             command = input.nextLine();
             if (command.equals("bye")) {
                 break;
+            } else if (command.equals("list")) {
+                for (int i = 0; i < numberOfCommands; i++) {
+                    System.out.println((i + 1) + ". " + commands[i]);
+                }
+                System.out.println("___________________________________");
+            } else {
+                commands[numberOfCommands] = command;
+                numberOfCommands++;
+                System.out.println("___________________________________");
+                System.out.println("added: " + command);
+                System.out.println("___________________________________");
             }
-            System.out.println("___________________________________");
-            System.out.println(command);
-            System.out.println("___________________________________");
         }
         System.out.println("___________________________________");
         System.out.println("Bye. Hope to see you again soon!");
